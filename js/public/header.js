9502ee1edb654c172700c08c70eb1511916fbedb
pingbo wang <pingbo wang>
2017-12-12 21:50:02 +0800

    update logo

--- js/public/header.js
+++ js/public/header.js
@@ -1,4 +1,4 @@
-var header = '<div class="fl topLogo"><a href="/"><img src="/images/fd_logo.png"></a></div>'+
+var header = '<div class="fl topLogo"><a href="/"><img src="/images/wpb_logo.png"></a></div>'+
 							'<h1 class="fl topTitle">云平台</h1>'+
 							'<div class="fr btnSelectBox zIndex100 topUser">'+
 								'<span class="btnSelect">'+
